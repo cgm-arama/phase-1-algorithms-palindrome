8ee6cfa11f21a366cfdabf38f838266f6e8b7059
Eric <eric.arama@student.moringaschool.com>
2023-04-12 09:10:22 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,88 +1,25 @@
-function isPalindrome(string) {
 
-  string = string.lower()
+let val = 'lal'
+isPalindrome(val)
+
+function isPalindrome(word) {
   // Write your algorithm here
-}
-
-console.log(isPalindrome("racecar")); // true
-
-
-console.log(isPalindrome("level")); // true
-
-
-console.log(isPalindrome("deified")); // true
-
-
-console.log(isPalindrome("hello")); // false
-
-
-console.log(isPalindrome("world")); // false
-
-
-console.log(isPalindrome("A man, a plan, a canal: Panama")); // true
-
-function isPalindrome(str) {
- 
-  str = str.toLowerCase().replace(/[^a-z]/g, "");
-  
-  
-  let start = 0;
-  let end = str.length - 1;
-  
- 
-  while (start < end) {
-    
-    if (str[start] !== str[end]) {
-      return false;
-    }
-    
-    start++;
-    
-    end--;
+  let normal = word
+  let reverse = (word) => {
+    let reversed = word.split('').reverse().join('')
+    console.log (reversed)
+    return reversed;
   }
- 
-  return true;
 
-  function isPalindrome(str) {
-    
-    str = str.toLowerCase().replace(/[^a-z]/g, "");
-    
-   
-    let start = 0;
-    let end = str.length - 1;
-    
-    
-    while (start < end) {
-     
-      if (str[start] !== str[end]) {
-        return false;
-      }
-      
-     
-      start++;
-      
-      
-      end--;
-    }
-    
-    
-    return true;
+  if(normal === reverse(word)){
+    return true
+  }else{
+    return false
   }
-  
 }
-
-
-/*
+/* 
   Add your pseudocode here
-
-  1. Convert the input string to lowercase and remove any non-letter characters.
-2. Initialize two pointers, one at the start of the string and the other at the end.
-3. While the two pointers haven't met:
-    a. If the characters at the two pointers don't match, return false.
-    b. Move the pointer at the start one position to the right.
-    c. Move the pointer at the end one position to the left.
-4. If the function has not returned false, return true.
-
+*/
 
 /*
   Add written explanation of your solution here
